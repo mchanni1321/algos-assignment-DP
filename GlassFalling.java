a87299b62c2d2a1bc992c1e2227d38cf6e2dd532
mchanni1321 <45618840+mchanni1321@users.noreply.github.com>
2019-04-03 02:09:39 -0400

    Completed GlassFalling.java

--- GlassFalling.java
+++ GlassFalling.java
@@ -6,7 +6,27 @@ public class GlassFalling {
   // Do not change the parameters!
   public int glassFallingRecur(int floors, int sheets) {
     // Fill in here and change the return
-    return 0;
+      if (floors == 1 || floors == 0) {
+		    return floors;
+	    }
+	    if (sheets == 1) {
+		    return floors;
+	    }
+	  
+	  int minimumTrials = Integer.MAX_VALUE;
+	  int result;
+	  
+	  for (int i = 0; i <= floors; i++) {
+		  result = 1 + Math.max(glassFallingRecur(0, floors - 1), glassFallingRecur(sheets - 1, floors));
+		  minimumTrials = Math.min(result, minimumTrials);
+		  
+		  if (result < minimumTrials) {
+			  minimumTrials = result;
+		  }
+	  }
+	  
+	  
+    return minimumTrials + 1;
   }
 
   // Optional:
@@ -19,6 +39,28 @@ public class GlassFalling {
   // Do not change the parameters!
   public int glassFallingBottomUp(int floors, int sheets) {
     // Fill in here and change the return
+    int glassFloor[][] = new int [sheets + 1][floors + 1];
+	  int result;
+	  int i, j;
+	  
+	  for (i = 0; i <= sheets; i++) {
+		  glassFloor[i][1] = 1;
+		  glassFloor[i][0] = 0;
+	  }
+	  for (j = 1; j <= floors; j++) {
+		  glassFloor[1][j] = j;
+	  }
+	  for (i = 2; j <= sheets; i++) {
+		  for (j = 2; j <= floors; j++) {
+			  glassFloor[i][j] = Integer.MAX_VALUE;
+			  for (int n = 1; n <= j; n++) {
+				  result = 1 + Math.max(glassFloor[i - 1][n - 1], glassFloor[i][j - n]);
+				  if (result < glassFloor[i][j])
+					  glassFloor[i][j] = result;
+			  }
+		  }
+	  }
+    return glassFloor[sheets][floors];
     return 0;
   }
 
